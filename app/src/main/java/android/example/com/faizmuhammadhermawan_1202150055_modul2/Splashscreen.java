3325b0b980625d73243c2f314e2dec0f0fbb4782
unknown <marcegha@gmail.com>
2018-02-18 20:52:05 +0700

    push pertama kali

---
+++ app/src/main/java/android/example/com/faizmuhammadhermawan_1202150055_modul2/Splashscreen.java
@@ -0,0 +1,24 @@
+package android.example.com.faizmuhammadhermawan_1202150055_modul2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        //https://stackoverflow.com/questions/5486789/how-do-i-make-a-splash-screen
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
